6a1ecc9daeda4d6ae0b748c4e74dfc3384e7224f
Shubham Prajapat <shubhamp.developer@gmail.com>
2022-06-26 11:31:24 +0530

    Added random() util function

--- utils/index.tsx
+++ utils/index.tsx
@@ -2,3 +2,7 @@ export const parsePracticeDate = (dateString: PracticeDate): Date => {
 	let [_day, _month, _year] = dateString.split("-");
 	return new Date(parseInt(_year), parseInt(_month) - 1, parseInt(_day));
 };
+
+export const random = (max: number, min = 0) => {
+	return min + Math.random() * (max - min);
+};
